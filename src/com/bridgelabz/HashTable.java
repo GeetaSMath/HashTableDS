6f63b790f5c9e55f699cdeb0a21976e63643c074
GeetaSMath <geetasmath@gmail.com>
2021-11-05 23:57:09 +0530

    Added: Remove avoidable word from the phrase

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -102,6 +105,18 @@ public class HashTable {
         }
     }
 
+    public void remWord(String str) {
+        Map<String, Integer> map = new TreeMap<>();
+
+        Iterator <String> word = map.keySet().iterator();
+        while (word.hasNext()){
+            if (word.next().equals(str)){
+                word.remove();
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
@@ -130,6 +145,11 @@ public class HashTable {
         String paraString = "paranoids are not paranoids because they are paranoid but they keep putting themselves deliberately into paranoid avoidable situation";
         System.out.println("Given Para String : " + paraString);
         hashTable.freqOfWords(paraString);
+
+        hashTable.remWord("they");
+
+
+
     }
 
 }
